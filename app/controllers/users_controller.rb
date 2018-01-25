79c3dabb04cf673ea1edd25817e2099a868ac6c3
Kazuhisa Yamamoto <ak.hisashi@gmail.com>
2018-01-25 19:16:36 +0900

    userインスタンスの取得方法を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :set_group
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def new
     @user = User.new
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = @group.users.find(params[:id])
     end
 
     def set_group
